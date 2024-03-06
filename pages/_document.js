282bf597b14d27044b87dcdefdef07ebae03c6ff
Olasubomi Awokoya <iamsubomi@gmail.com>
2024-03-06 09:09:04 -0800

    adding next thirdpartypackage and layout root file for google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -25,31 +25,6 @@ export default function Document() {
                         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
                     /> */}
 
-                    {/* <!--
-                    Notice the use of %PUBLIC_URL% in the tags above.
-                    It will be replaced with the URL of the `public` folder during the build.
-                    Only files inside the `public` folder can be referenced from the HTML.
-
-                    Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
-                    work correctly both with client-side routing and a non-root public URL.
-                    Learn how to configure a non-root public URL by running `npm run build`.
-    --> */}
-
-
-                    {/* <!-- Google Tag Manager --> */}
-                    {/* <script>
-                        (function (w, d, s, l, i) {
-                            w[l] = w[l] || [];
-                        w[l].push({"gtm.start": new Date().getTime(), event: "gtm.js" });
-                        var f = d.getElementsByTagName(s)[0],
-                        j = d.createElement(s),
-                        dl = l != "dataLayer" ? "&l=" + l : "";
-                        j.async = true;
-                        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-                        f.parentNode.insertBefore(j, f);
-      })(window, document, "script", "dataLayer", "GTM-NRQ9VM4");
-                    </script> */}
-                    {/* <!-- End Google Tag Manager --> */}
 
                     {/* <!-- Google Ad Sense --> */}
                     <script
