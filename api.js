16806e5ea80f5f852464a38c2a77156e1e002eed
Olasubomi O Awokoya <olasubomi.awokoya@hotmail.com>
2024-05-14 16:53:36 -0700

    Update api.js to include requests to dev server from dev live production for testing

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
  export const BASE_URL_LIVE = `https://chopchowsd.onrender.com/api`
+ export const BASE_URL_LIVE_DEV = `https://chopchowserver2.onrender.com/api`
  export const BASE_URL_DEV = `http://localhost:5000/api/`
 
 
