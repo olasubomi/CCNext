d9b35b1112acbafd615b43b57d7d54948cf44944
Maroprecious <maroumukoro98@gmail.com>
2024-06-21 17:05:12 +0100

    api to remove sub admin integrated

--- src/components/management/subadmins.jsx
+++ src/components/management/subadmins.jsx
@@ -2,19 +2,42 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import styles from "../../components/dashboard/management.module.css";
 import { TbDotsVertical } from "react-icons/tb";
-import { addUserToStore } from "../../actions/UserStore";
+import { addUserToStore, removeUserFromStore } from "../../actions/UserStore";
 import { UserIcon } from "../icons";
 import { toast } from "react-toastify";
+import { AiOutlineClose } from "react-icons/ai";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 export const SubAdmins = ({ storeId, storeData, handleGetStore }) => {
   const dispatch = useDispatch();
+  const [show, setShow] = useState(false);
   const [formState, setFormState] = useState({
     first_name: "",
     last_name: "",
     email: "",
     number: "",
   });
+  const [selectedAdmin, setSelectedAdmin] = useState(null);
 
+  const handleRemove = async () => {
+    try {
+      if (!selectedAdmin) {
+        console.error("No subadmin selected to remove.");
+        return;
+      }
+      await dispatch(removeUserFromStore(storeId, selectedAdmin));
+      toast.success("Sub admin removed successfully");
+      handleGetStore();
+    } catch (error) {
+      console.error("Error removing sub admin:", error);
+      toast.error("Failed to remove sub admin. Please try again later.");
+    } finally {
+      setShow(false);
+    }
+  };
+
+  console.log(selectedAdmin, "one store data");
+  console.log(storeData, "storeData");
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormState({
@@ -28,14 +51,14 @@ export const SubAdmins = ({ storeId, storeData, handleGetStore }) => {
       toast.success("Sub admin added successfully");
       handleGetStore();
     } catch (error) {
-      console.log(error);
+      console.log(error, "erryy");
+      toast.error(error?.response?.data?.message?.messsage);
     }
   };
-  console.log(storeData, "formstate");
 
   return (
     <div>
-      <div className={styles.flex}>
+      {/* <div className={styles.flex}>
         <h5>Admins</h5>
       </div>
       <div className={styles.subadmin}>
@@ -51,13 +74,37 @@ export const SubAdmins = ({ storeId, storeData, handleGetStore }) => {
         <div className={styles.center}>
           <TbDotsVertical color="#949494" size={20} />
         </div>
-      </div>
+      </div> */}
       <div className={styles.flex} style={{ marginTop: "2rem" }}>
         <h5>Sub Admins</h5>
       </div>
-      <div style={{ gap: "2rem", display: "flex", flexDirection: "column" }}>
+      <div
+        style={{
+          gap: "2rem",
+          display: "flex",
+          flexDirection: "column",
+          position: "relative",
+        }}
+      >
         {storeData?.store_sub_admins.map((elem) => (
           <div className={styles.subadmin}>
+            {show && selectedAdmin == elem._id && (
+              <div className={styles.removeAdmin}>
+                <div
+                  className={styles.closeContainer}
+                  onClick={() => setShow(false)}
+                >
+                  <AiOutlineClose className={styles.closeIcon} />
+                </div>
+                <div
+                  style={{ display: "flex", alignItems: "center", gap: "4px" }}
+                  onClick={() => handleRemove()}
+                >
+                  <RiDeleteBin6Line style={{ marginTop: "1rem" }} color="red" />
+                  <p>Remove sub-admin</p>
+                </div>
+              </div>
+            )}
             <div className={styles.flexstart}>
               <div className={styles.user}>
                 {elem.profile_picture ? (
@@ -73,7 +120,13 @@ export const SubAdmins = ({ storeId, storeData, handleGetStore }) => {
                 <p className={styles.role}>Sub Admin</p>
               </div>
             </div>
-            <div className={styles.center}>
+            <div
+              className={styles.center}
+              onClick={() => {
+                setSelectedAdmin(elem._id);
+                setShow(true);
+              }}
+            >
               <TbDotsVertical color="#949494" size={20} />
             </div>
           </div>
