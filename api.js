db36b0e21b86deb823ba02fd4bac7b562f940fe1
Ayowalexy <seinde4@gmail.com>
2024-08-19 17:06:38 +0100

    render tests

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
  export const BASE_URL_LIVE = `https://chopchowsd.onrender.com/api`
- export const BASE_URL_DEV = `http://localhost:5000/api`
+ export const BASE_URL_DEV =  "https://chopchowsd-dawy.onrender.com/api" // `http://localhost:5000/api`
 
 
 
